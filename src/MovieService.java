dc33461af2c353341abb98cb77f2f01947cc7bf0
Filip Dimitrijevic <filipdi@kth.se>
2023-01-24 18:12:53 +0100

    all changes

---
+++ src/MovieService.java
@@ -0,0 +1,23 @@
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+/**
+ *  This class should contain methods that handle the 
+ * business logic of the application. For example, 
+ * it may contain methods to search for movies or books based on certain criteria, 
+ * or to retrieve information about a specific movie or book.
+ */
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+    
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+    
+    public List<Movie> findMoviesByTitle(String title) {
+        return movieRepository.findByTitle(title);
+    }
+}
+
