7a64cba918033131461fae72079e15e0fa8ed6fd
Filip Dimitrijevic <filipdi@kth.se>
2023-01-24 22:08:28 +0100

    add App.js

---
+++ web/App.js
@@ -0,0 +1,10 @@
+const getMovies = () => {
+  fetch('http://localhost:8080/movies')
+    .then(response => response.json())
+    .then(movies => {
+      // Do something with the list of movies
+    })
+    .catch(error => {
+      console.error('Error fetching movies:', error);
+    });
+}
