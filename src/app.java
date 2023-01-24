e0d5d52f49d9a2fd800c82cae3ca6c13286b2c9d
Filip Dimitrijevic <filipdi@kth.se>
2023-01-24 16:36:29 +0100

    adding App.java and MainController.java

--- src/app.java
+++ src/app.java
@@ -1,3 +1,15 @@
-public class app {
-    
-}
+/**
+ * A class that contains the main method for the application. The purpose is to create
+ * simple web application that allows users to search for and view information about movies or books.
+ * 
+ * @author Filip Dimitrijevic
+ * @date 2023-01-23
+ */
+
+ @SpringBootApplication
+ public class App {
+     public static void main(String[] args) {
+         SpringApplication.run(App.class, args);
+     }
+ }
+ 
