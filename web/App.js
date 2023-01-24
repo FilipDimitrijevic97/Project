774ccd1dd0e13adc69ea7f3309e77e3aefced2c3
Filip Dimitrijevic <filipdi@kth.se>
2023-01-24 22:36:47 +0100

    add: web/App.js

--- web/App.js
+++ web/App.js
@@ -1,10 +1,10 @@
 const getMovies = () => {
   fetch('http://localhost:8080/movies')
     .then(response => response.json())
-    .then(movies => {
-      // Do something with the list of movies
-
-    })
+    .then(movies => movies.map(movie => movie.title))
+    .then(titles => console.log('Movies:', titles))
+    
+    // TODO: Handle errors
     .catch(error => {
       console.error('Error fetching movies:', error);
     });
