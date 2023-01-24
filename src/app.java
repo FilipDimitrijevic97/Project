6975c84e0468bbb0189643eaad31317b9a9c4303
Filip Dimitrijevic <filipdi@kth.se>
2023-01-24 20:40:42 +0100

    change

--- src/app.java
+++ src/app.java
@@ -1,4 +1,4 @@
-package Users.filipdimitrijevic.KTH.project.Project.src;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
